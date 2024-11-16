90dfbdd65cf15b2d0f7ae6286000dabad62bbfb6
Marquinhos Silvino <marquinhossilvino89@gmail.com>
2024-11-16 08:50:59 -0300

    Create teste_simples_aluno.ino

---
+++ teste_simples_aluno/teste_simples_aluno.ino
@@ -0,0 +1,18 @@
+// Esse é um teste feito pelo Davi Dominiski
+void setup() {
+
+pinMode(12,OUTPUT); 
+pinMode(7,OUTPUT);  
+pinMode(3,OUTPUT);  
+
+}
+
+void loop() {
+ delay(3000);
+ digitalWrite(12,HIGH);// high = ligado
+ delay(3000);
+ digitalWrite(7,HIGH); 
+  delay(3000);
+ digitalWrite(3,HIGH); 
+  delay(1000);
+}
